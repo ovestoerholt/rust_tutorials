0a0f4ba8a86126e22790a0f15522deb4017bf422
Ove Stoerholt <ove@inforte.no>
2024-09-23 20:07:46 +0200

    Added route / pointing to hello_world function

--- shuttle_axum_getting_started/src/main.rs
+++ shuttle_axum_getting_started/src/main.rs
@@ -2,8 +2,12 @@ use axum::{routing::get, Router};
 
 #[tokio::main]
 async fn main() {
-    let app = Router::new().route("/", get(|| async { "Hello world" }));
+    let app = Router::new().route("/", get(hello_world));
 
     let listener = tokio::net::TcpListener::bind("0.0.0.0:4000").await.unwrap();
     axum::serve(listener, app).await.unwrap();
+}
+
+async fn hello_world() -> &'static str {
+    "Hello world!"
 }
