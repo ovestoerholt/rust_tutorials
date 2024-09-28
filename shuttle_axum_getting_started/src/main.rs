505f82192c80201a588bf3e336b67e24037d66df
Ove Stoerholt <ove@inforte.no>
2024-09-28 09:06:09 +0200

    Returning list of json data

--- shuttle_axum_getting_started/src/main.rs
+++ shuttle_axum_getting_started/src/main.rs
@@ -1,9 +1,16 @@
-use axum::{routing::get, Json, Router};
+use axum::{
+    http::StatusCode,
+    response::{IntoResponse, Response},
+    routing::get,
+    Json, Router,
+};
 use serde::{Deserialize, Serialize};
 
 #[tokio::main]
 async fn main() {
-    let app = Router::new().route("/", get(hello_world));
+    let app = Router::new()
+        .route("/", get(hello_world))
+        .route("/list", get(list_of_json_data));
 
     let listener = tokio::net::TcpListener::bind("0.0.0.0:4000").await.unwrap();
     axum::serve(listener, app).await.unwrap();
@@ -20,4 +27,29 @@ async fn hello_world() -> Json<Hello> {
 
 struct Hello {
     message: String,
+}
+
+
+async fn list_of_json_data() -> ApiResponse {
+    let msg0 = Hello{ message: "Hei og hei".to_string(),};
+    let msg1 = Hello{ message: "Hoj og hoj".to_string(),};
+    let v = vec![msg0, msg1];
+    ApiResponse::JsonData(v)
+}
+
+
+enum ApiResponse {
+    Ok,
+    Created,
+    JsonData(Vec<Hello>),
+}
+
+impl IntoResponse for ApiResponse {
+    fn into_response(self) -> Response {
+        match self {
+            ApiResponse::Ok => (StatusCode::OK).into_response(),
+            ApiResponse::Created => (StatusCode::CREATED).into_response(),
+            ApiResponse::JsonData(data) => (StatusCode::OK, Json(data)).into_response(),
+        }
+    }
 }
