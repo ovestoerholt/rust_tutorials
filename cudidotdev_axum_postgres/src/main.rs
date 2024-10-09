2b68ebcaaadd0ce06390423daa89e4d494930f6b
Ove Stoerholt <ove@inforte.no>
2024-10-09 16:29:38 +0200

    cudidotdev_axum_postgres: - Added tasks table using migrations - Refactored to use async main - Running migrations when starting program - Added SQLx build-script

--- cudidotdev_axum_postgres/src/main.rs
+++ cudidotdev_axum_postgres/src/main.rs
@@ -1,8 +1,21 @@
 use dotenv::dotenv;
-use std::env;
+use std::{env, error::Error};
 
-fn main() {
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn Error>> {
     dotenv().ok();
     let database_url = env::var("DATABASE_URL").expect("DATABASE_URL is not set");
     println!("DATABASE_URL: {}", database_url);
+
+    // Create database connections pool
+    let pool = sqlx::postgres::PgPool::connect(&database_url)
+        .await
+        .expect("Failed to connect to the database");
+
+    // Run existing database migrations
+    sqlx::migrate!("./migrations").run(&pool)
+        .await
+        .expect("Failed to run migrations...");
+
+    Ok(())
 }
