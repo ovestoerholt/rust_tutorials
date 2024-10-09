0172cc8329722bd522a504d957a8b60604b257a0
Ove Stoerholt <ove@inforte.no>
2024-10-09 17:30:35 +0200

    cudidotdev_axum_postgres: - Added Hello World Axum app

--- cudidotdev_axum_postgres/src/main.rs
+++ cudidotdev_axum_postgres/src/main.rs
@@ -1,21 +1,38 @@
+use axum::{routing::get, Router};
 use dotenvy::dotenv;
-use std::{env, error::Error};
+use sqlx::postgres::PgPoolOptions;
+use tokio::net::TcpListener;
+use std::env;
 
 #[tokio::main]
-async fn main() -> Result<(), Box<dyn Error>> {
-    dotenv().ok();
-    let database_url = env::var("DATABASE_URL").expect("DATABASE_URL is not set");
+async fn main() -> () {
+    dotenv().expect("Unable to access .env file");
+    // Read server address from .env/environment. Fallback to 127.0.0.1:3000
+    let server_address =
+        env::var("SERVER_ADDRESS").unwrap_or("127.0.0.1:3000".to_owned());
+    let database_url =
+        env::var("DATABASE_URL").expect("Unable to read DATABASE_URL environment variable");
     println!("DATABASE_URL: {}", database_url);
 
     // Create database connections pool
-    let pool = sqlx::postgres::PgPool::connect(&database_url)
+    let db_pool = PgPoolOptions::new()
+        .max_connections(16)
+        .connect(&database_url)
         .await
         .expect("Failed to connect to the database");
 
     // Run existing database migrations
-    sqlx::migrate!("./migrations").run(&pool)
+    sqlx::migrate!("./migrations")
+        .run(&db_pool)
         .await
         .expect("Failed to run migrations...");
 
-    Ok(())
+    // Create Axum TCP listener
+    let listener = TcpListener::bind(server_address).await.expect("Could not create a TCP listener");
+
+    println!("Listening on {}", listener.local_addr().unwrap());
+
+    let app = Router::new().route("/", get(|| async {"Hello World!"}));
+
+    axum::serve(listener, app).await.expect("Error serving application");
 }
